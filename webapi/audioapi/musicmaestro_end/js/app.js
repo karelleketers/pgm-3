5582b66ffdeb1bcc79058c4ff0ce5368e746e4e6
Frederick Roegiers <frederick@rogerthat.be>
2021-02-11 14:08:08 +0100

    musicmaestro halfway

---
+++ webapi/audioapi/musicmaestro_end/js/app.js
@@ -0,0 +1,101 @@
+const AudioContext = window.AudioContext || window.webkitAudioContext;
+const audioContext = new AudioContext();
+
+// const sound = audioContext.createOscillator();
+// const volume = audioContext.createGain();
+// sound.frequency.value = 5000;
+// sound.start();
+// volume.gain.value = 0;
+// sound.connect(volume).connect(audioContext.destination);
+// sound.connect(audioContext.destination);
+
+let source;
+
+let audio = null;
+let audioSource = null;
+
+const gainNode = audioContext.createGain();
+
+// const panNode = audioContext.createStereoPanner();
+// const filterNode = audioContext.createBiquadFilter();
+// filterNode.type = filterNode.LOWPASS;
+// filterNode.frequency.value = 3000;
+
+const app = {
+  init() {
+    this.cacheElements();
+    this.registerListeners();
+  },
+
+  bufferSong() {
+    const selectedSong = this.$songselector.value;
+
+    audio = new Audio(`./audio/${selectedSong}.mp3`);
+    console.log(audio);
+    audioSource = audioContext.createMediaElementSource(audio);
+    audioSource // bron
+      .connect(gainNode) // modificatie 1: volume
+      .connect(audioContext.destination); // eindbestemming
+  },
+  cacheElements() {
+    this.$audioEl = document.querySelector("audio");
+    this.$songselector = document.querySelector("#songselector");
+    this.$playBtn = document.querySelector("#play");
+    this.$stopBtn = document.querySelector("#stop");
+    this.$volumeInp = document.querySelector("#volume");
+    this.$panInp = document.querySelector("#pan");
+    this.$filterInp = document.querySelector("#filter");
+  },
+  registerListeners() {
+    this.$songselector.addEventListener("change", () => {
+      this.stopMusic();
+    });
+
+    this.$playBtn.addEventListener("click", () => {
+      // check if context is in suspended state (autoplay policy)
+      audioContext.state === "suspended" ? audioContext.resume() : "";
+      this.playMusic();
+    });
+
+    this.$stopBtn.addEventListener("click", () => {
+      this.stopMusic();
+    });
+
+    this.$volumeInp.addEventListener("input", function () {
+      // change gain
+      gainNode.gain.value = this.value;
+    });
+
+    this.$panInp.addEventListener("input", function () {
+      // change pan
+    });
+
+    this.$filterInp.addEventListener("input", function () {
+      // change filter
+    });
+  },
+  playMusic() {
+    this.$playBtn.disabled = true;
+    this.$stopBtn.disabled = false;
+
+    this.bufferSong();
+
+    audio.play();
+
+    //  this.$audioEl.play();
+  },
+  stopMusic() {
+    this.$stopBtn.disabled = true;
+    this.$playBtn.disabled = false;
+
+    audio.pause();
+    audio.currentTime = 0;
+
+    // this.$audioEl.pause(); // pauzeren
+    // this.$audioEl.currentTime = 0; // terug starten op positie 0 in tijdslijn
+    // audio.disconnect();
+  },
+};
+
+document.addEventListener("DOMContentLoaded", app.init());
+/* made with <3 4 PGM-3 */
